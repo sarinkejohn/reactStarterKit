4a8b566c8703e9886c5ff1d52d4f1022c82150dc
sarinke john <sarinke.john@chakray.com>
2025-09-02 12:18:57 +0300

    merged

---
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -0,0 +1,11 @@
+import productsController from './productsController'
+import Settings from './Settings'
+import Auth from './Auth'
+
+const Controllers = {
+    productsController,
+    Settings,
+    Auth,
+}
+
+export default Controllers
